5a023750f2c5a9a32d8ce888d0c3e64ed72a146b
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2025-05-21 18:43:26 +0200

    Add virtual screen reader tests (#4)
    * Add virtual screen reader tests
    
    * Add virtual screenreader to ci

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    include: ['./test/testing-library/**/*.test.jsx', './test/testing-library/**/*.test.js'],
-    environment: 'happy-dom',
-    setupFiles: 'test/testing-library/vitest.setup.js',
-    passWithNoTests: true,
-    reporters: ['default', 'junit'],
-    outputFile: './reports/testing-library.xml',
-  }
+  plugins: [react()]
 })
